62678e783063f28dc34efe6d940712f401f2b20a
alderjunio <alder.junio@gmail.com>
2024-07-26 13:04:16 -0400

    edit api dados pessoais

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,7 @@
 
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/alderjunio/pagina-de-portifolio/main/data/profile.json'
+    const fetching = await fetch(url)
+    return await fetching.json()
+}
+
